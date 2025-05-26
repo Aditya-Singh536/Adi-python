0d938c9d914212b5ab2922d7cd8c53107de9c622
Aditya <129666207+Aditya-Singh536@users.noreply.github.com>
2025-05-26 15:38:24 +0530

    Create Tally_Counter.py

---
+++ Tally_Counter.py
@@ -0,0 +1,73 @@
+import customtkinter as ctk
+
+
+app = ctk.CTk()
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
+app.title("Tally Counter!")
+app.geometry("500x400")
+
+count = [0]
+
+label = ctk.CTkLabel(app, text=str(count[0]),
+                     font=("Arial", 60),
+                     text_color="white")
+label.pack(pady=20)
+
+def add(): 
+    count[0] += 1
+    label.configure(text=str(count[0]))
+
+def minus():
+    if count[0] > 0:
+        count[0] -= 1
+        label.configure(text=str(count[0]))
+    else:
+        root = ctk.CTk()
+        root.geometry("300x150")
+        error_label = ctk.CTkLabel(root, text="Can't go in negative digits!!",
+                                   text_color="red",
+                                   font=("Arial", 20))
+        error_label.pack(pady=50)
+
+def cls():
+    count[0] = 0
+    label.configure(text=str(count[0]))
+
+button_frame = ctk.CTkFrame(app)
+button_frame.pack(pady=20)
+
+button = ctk.CTkButton(button_frame, text="+",
+                       corner_radius=50,
+                       font=("Arial", 100),
+                       command=add,
+                       height=150,
+                       width=150,
+                       fg_color="aqua",
+                       text_color="black",
+                       hover_color="blue")
+button.pack(side="left", padx=10)
+
+button2 = ctk.CTkButton(button_frame, text="-",
+                        corner_radius=50,
+                        font=("Arial", 100),
+                        command=minus,
+                        height=150,
+                        width=150,
+                        fg_color="aqua",
+                        text_color="black",
+                        hover_color="blue")
+button2.pack(side="left", padx=10)
+
+button3 = ctk.CTkButton(app, text="Reset",
+                        corner_radius=25,
+                        text_color="black",
+                        font=("Arial", 15),
+                        command=cls,
+                        height=50,
+                        width=100,
+                        fg_color="orange",
+                        hover_color="red",)
+button3.pack(pady=10)
+
+app.mainloop()
