c252b968191ddd6443230cde0fea13ac0bcda0c1
Aditya <129666207+Aditya-Singh536@users.noreply.github.com>
2025-06-27 17:36:13 +0530

    Update KBC.py

--- KBC.py
+++ KBC.py
@@ -1,23 +1,36 @@
-q = {'Q1':'Slowest animal in the world',
-     'Q2':'Fastest car'}
-a = {'A1':'a.Sloth  b.Turtle',
-     'A2':'a.Chiron b.Jesko'}
+def kbc_game():
+    question = {"q1": "\nSlowest animal in the world", "q2": "\nFastest car"}
+    answer = {"a1": "a.Sloth  b.Turtle", "a2": "a.Chiron b.Jesko"}
 
-c = q[input('Which question Q1 or Q2 :')]
+    ask = input("Which question Q1 or Q2 :").lower().strip()
 
-if c == q['Q1']:
-    print(q['Q1'])
-    print(a['A1'])
-    x = input('Ans:')
-    if x == 'Sloth':
-        print('Correct')
-    elif x == 'Turtle':
-        print('Incorrect')
-elif c == q['Q2']:
-    print(q['Q2'])
-    print(a['A2'])
-    y = input('Ans:')
-    if y == 'Chiron':
-        print('Incorrect')
-    elif y == 'Jesko':
-        print('Correct')
+    while ask == "q1" or ask == "q2":
+        if ask == "q1":
+            print(question["q1"])
+            print(answer["a1"])
+            ask = input("Ans:").lower().strip()
+            if ask == "sloth" or "a.sloth" or "a":
+                print("\nCorrect")
+            elif ask == "turtle" or "b.turtle" or "b":
+                print("\nIncorrect")
+        elif ask == "q2":
+            print(question["q2"])
+            print(answer["a2"])
+            ask = input("Ans:").lower().strip()
+            if ask == "chiron" or "a.chiron" or "a":
+                print("\nIncorrect")
+            elif ask == "Jesko" or "b.jesko" or "b":
+                print("\nCorrect")
+        else:
+            raise Exception("**Invalid Input**")
+
+kbc_game()
+
+ak = input("\nDo you want to play again? (yes/no): ").lower().strip()
+if ak == "yes":
+    print("Restarting the game...")
+    kbc_game()
+elif ak == "no":
+    print("Thank you for playing!")
+else:
+    print("**Invalid Input**")
